2cff086585893f6fd5880a3f9eca0f51ed5b367a
Caspinol <kristhesavage@gmail.com>
2015-08-08 13:00:17 +0100

    parseing and grouping CDRs

--- billing_engine/mediation/grnti_parser.py
+++ billing_engine/mediation/grnti_parser.py
@@ -1,23 +1,27 @@
-import sys
 
-cdr_line = {}
 
 def parse_line(l):
      line = l.rstrip("\n").split(" ")
 
-     # extract interesting bits from line
-     cdr_line['account'] = line[2];
-     cdr_line['direction'] = line[3];
-     cdr_line['call_date'] = line[4] + " "+line[5];
-     cdr_line['call_length'] = line[6];
-     cdr_line['calling_num_1'] = line[15];
-     cdr_line['calling_num_2'] = line[17];
-     cdr_line['called_num_1'] = line[21];
-     cdr_line['called_num_2'] = line[25];
-     cdr_line['operator_id'] = line[29];
-     cdr_line['in_trunk'] = line[31];
-     cdr_line['out_trunk'] = line[32];
+     # Extract interesting bits from line.
+     cdr_line = {
+          'cdr_type': line[0].split("$")[0], 'account': line[2],
+          'direction': line[3], 'call_date': ' '.join([line[4],line[5]]),
+          'call_length': line[6], 'calling_num_1': line[15], 'calling_num_2': line[17],
+          'called_num_1': line[21], 'called_num_2': line[25], 'operator_id': line[29],
+          'in_trunk': line[31], 'out_trunk': line[32]
+          }
 
-     #now lets find the d-pkg
-     
-     print cdr_line
+     # Now lets find the d-pkg.
+     for idx1, field1 in enumerate(line[32:], start=32):
+          if field1.startswith("D"):
+               cdr_line['d-pkg'] = ''.join([line[idx1+1], line[idx1+2]])
+               idx1 += 2
+               break
+
+     for idx2, field2 in enumerate(line[idx1:], start=idx1):
+          if field2.startswith("H"):
+               cdr_line['h-pkg'] = line[idx2+2]
+               break
+     # All done here.     
+     return cdr_line
--- billing_engine/mediation/mediate.py
+++ billing_engine/mediation/mediate.py
@@ -8,12 +8,40 @@ def main(argv):
     if len(argv) < 2:
         print "Need to give the filename"
         return
-    
+
+    bucket = {}
+    queue = []
     with open(argv[1], 'r') as grnti_file:
         for line in grnti_file:
             
-            parse_line(line)
+            cdr = parse_line(line)
+            if cdr['d-pkg'] in bucket:
+                # Ensure that TR cdr is always first.
+                if cdr['cdr_type'] == 'TR':
+                    cdr_pair = (cdr, bucket[cdr['d-pkg']])
+                else:
+                    cdr_pair = (bucket[cdr['d-pkg']], cdr)
+
+                del bucket[cdr['d-pkg']]
+                queue.append(cdr_pair)
+
+            else:
+                bucket[cdr['d-pkg']] = cdr
+
+
+    # Now empty the buckets with "no-pairs" to queue
+    for nopair in bucket.values():
+        # Ensure that TR cdr is always first.
+        if nopair['cdr_type'] == 'TR':
+            cdr_pair = (nopair, None)
+        else:
+            cdr_pair = (None, nopair)
             
+        queue.append(cdr_pair)
+        
+    print queue            
+
+                    
             
 if __name__ == '__main__':
     main(sys.argv)
