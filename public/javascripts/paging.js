d48cdfe056458ed8a76cb9e3d9cf9ccb37b41afb
Caspinol <kristhesavage@gmail.com>
2015-08-15 15:23:20 +0100

    Added this embarrasing offset generation to pagination

--- public/javascripts/paging.js
+++ public/javascripts/paging.js
@@ -1,4 +1,13 @@
 var generate_paging = function(st, div){
+
+    var offset = 0;
+    for(x=0;x<1000; x+=10){
+        if(Number(st.numpage) < x){
+            break;
+        }else{
+            offset = x;
+        }
+    }
     
     div.append("<ul class='pagination'></ul>");
 
@@ -7,19 +16,18 @@ var generate_paging = function(st, div){
                                 +'<span aria-hidden="true">&laquo;</span></a></li>');
     }else{
         $(".pagination").append('<li><a href="/mediation_page/'+ st.perpage +'/'
-                                + (st.numpage-1) +'/'+st.sdate+'/'+st.edate+'/'+st.valid+'" aria-label="Previous">'
+                                + (Number(st.numpage)-1) +'/'+st.sdate+'/'+st.edate+'/'+st.valid+'" aria-label="Previous">'
                                 +'<span aria-hidden="true">&laquo;</span></a></li>');
     }
-    for (i = 1; i < 10; i++) {
-        if(i == st.numpage){
-            $(".pagination").append('<li class="active"><a id="ov-paging-active" href="/mediation_page/'
-                                    + st.perpage +'/'+ i +'/'+st.sdate+'/'+st.edate+'/'+st.valid+'">'+ i +'</a></li>');
-        }else{
-            $(".pagination").append('<li><a href="/mediation_page/'
-                                    + st.perpage +'/'+ i +'/'+st.sdate+'/'+st.edate+'/'+st.valid+'">'+ i +'</a></li>');
-        }
+    for (i = 1+offset; i <= 20+offset; i++) {
+        
+        var active  = (i == st.numpage)? "active": "";
+        
+        $(".pagination").append('<li class="'+active+'"><a href="/mediation_page/'
+                                + st.perpage +'/'+ i +'/'
+                                +st.sdate+'/'+st.edate+'/'+st.valid+'">'+ i +'</a></li>');
     }
     $(".pagination").append('<li><a href="/mediation_page/'+ st.perpage +'/'
-                            + (st.numpage+1) +'/'+st.sdate+'/'+st.edate+'/'+st.valid+'" aria-label="Previous">'
+                            + (Number(st.numpage)+1) +'/'+st.sdate+'/'+st.edate+'/'+st.valid+'" aria-label="Previous">'
                             +'<span aria-hidden="true">&raquo;</span></a></li>');
 }
